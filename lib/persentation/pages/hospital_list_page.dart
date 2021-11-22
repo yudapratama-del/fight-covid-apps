48dddad8bf978f8ab2adb2b857b07000f3e50df6
Kai-69 <ichihatsu1@gmail.com>
2021-11-22 11:52:29 +0700

    add hospital list page

---
+++ lib/persentation/pages/hospital_list_page.dart
@@ -0,0 +1,20 @@
+import 'package:capstone_apps/persentation/widgets/hospital_item.dart';
+import 'package:flutter/material.dart';
+
+class HospitaleListPage extends StatelessWidget {
+  const HospitaleListPage({Key? key}) : super(key: key);
+
+  static List dataTest = [1];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Daftar Rumah Sakit"),),
+      body: ListView.builder(
+          itemCount: dataTest.length,
+          itemBuilder: (BuildContext context, int index) {
+            return HospitalItem();
+          }),
+    );
+  }
+}
