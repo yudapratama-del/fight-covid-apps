12dbfba483fd650c478de3fa1726d7acb241407d
Kai-69 <ichihatsu1@gmail.com>
2021-11-22 12:05:12 +0700

    add widget header in hospital detail page

--- lib/persentation/pages/hospital_detail_page.dart
+++ lib/persentation/pages/hospital_detail_page.dart
@@ -6,6 +6,61 @@ class HospitalDetailPage extends StatelessWidget {
 
   static List dataTest = [1, 1, 1, 1, 1, 1];
 
+  Widget Header(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 7,
+              blurRadius: 7,
+              offset: Offset(0, 0),
+            )
+          ],
+          borderRadius:
+              BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [kDeepGreen, kLightGreen],
+          )),
+      height: 250,
+      child: Column(
+        children: [
+          Container(
+              margin: EdgeInsets.only(top: 20, bottom: 10),
+              child: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage("assets/images/hospital_illustration.png"))),
+          Container(
+            child: Text(
+              "Nama",
+              style: kHeading5.copyWith(color: kWhite),
+            ),
+          ),
+          Container(
+            child: Text(
+              "Alamat",
+              style: kHeading6.copyWith(color: kWhite),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 15),
+            child: Text(
+              "Tlpn",
+              style: kSubtitle.copyWith(color: kWhite),
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(primary: kWhite, onPrimary: kDeepGreen),
+            child: Text("Go To Gmaps", style: kBodyText),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+
   
 
   @override
