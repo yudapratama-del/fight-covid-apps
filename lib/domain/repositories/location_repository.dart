124bb0d79acc3f47cbf83a7845670147a707772c
MUHAMMAD ZIAD AFLIAN <muhammadziadalfian07@gmail.com>
2021-11-23 06:19:11 +0800

    Add Repository & Usecase City

--- lib/domain/repositories/location_repository.dart
+++ lib/domain/repositories/location_repository.dart
@@ -1,7 +1,9 @@
 import 'package:capstone_apps/common/failure.dart';
+import 'package:capstone_apps/domain/entities/city.dart';
 import 'package:capstone_apps/domain/entities/province.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class LocationRepository {
   Future<Either<Failure, List<Province>>> getDataProvince();
+  Future<Either<Failure, List<City>>> getDataCity(String provinceId);
 }
