110bad9dbe4261590d01ad12108fc2c49c0ee1f7
Kai-69 <ichihatsu1@gmail.com>
2021-11-22 10:44:15 +0700

    add Google Font nunito and Color Green

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color kPurplePrimary = Color(0xff6B4DF7);
 Color kPurpleSecondary = Color(0xff6D4EFA);
@@ -7,4 +9,37 @@ Color kGreySecondary = Color(0xffAAAAAA);
 Color kWhite = Color(0xffFFFFFF);
 Color kBlack = Color(0xff121212);
 Color kRed = Color(0xffFF1800);
-Color kGreen = Color(0xff2ECC71);
+
+Color kDeepGreen = Colors.green.shade600;
+Color kLightGreen = Colors.green.shade300;
+
+const double margin = 24;
+
+final TextStyle kHeading4 = GoogleFonts.nunito(
+  fontSize: 30,
+  fontWeight: FontWeight.w700,
+);
+
+final TextStyle kHeading5 = GoogleFonts.nunito(
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 0.15,
+);
+
+final TextStyle kHeading6 = GoogleFonts.nunito(
+  fontSize: 18,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.15,
+);
+
+final TextStyle kSubtitle = GoogleFonts.nunito(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.15,
+);
+
+final TextStyle kBodyText = GoogleFonts.nunito(
+  fontSize: 13,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.25,
+);
