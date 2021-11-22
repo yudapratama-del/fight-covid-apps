5ddd6122bb8a4f05ff0b8de89b9ef80b9441caa8
Kai-69 <ichihatsu1@gmail.com>
2021-11-22 12:07:40 +0700

    add widget custom app bar in news detail page

--- lib/persentation/pages/news_detail_page.dart
+++ lib/persentation/pages/news_detail_page.dart
@@ -8,6 +8,37 @@ class NewsDetailPage extends StatelessWidget {
 
   
 
+  Widget CustomAppBar() {
+    return Card(
+      color: kDeepGreen,
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(16.0),
+        ),
+      ),
+      margin: EdgeInsets.all(0),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            icon: Icon(Icons.arrow_back, color: kWhite,),
+            onPressed: () {
+              print("Back");
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Text(
+              'Fight Covid',
+              style: kHeading6.copyWith(color: kWhite),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
