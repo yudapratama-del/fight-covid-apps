a539233ce0e1c7c0c6e7015762692827858e1694
Kai-69 <ichihatsu1@gmail.com>
2021-11-22 12:08:39 +0700

    add widget WebView in news detail page

--- lib/persentation/pages/news_detail_page.dart
+++ lib/persentation/pages/news_detail_page.dart
@@ -6,7 +6,12 @@ import 'package:webview_flutter/webview_flutter.dart';
 class NewsDetailPage extends StatelessWidget {
   const NewsDetailPage({Key? key}) : super(key: key);
 
-  
+  final String contohURL = "https://www.inews.id/lifestyle/health/1-dari-10-orang-di-indonesia-penderita-diabetes-wamenkes-dante-tertinggi-di-maluku-utara";
+  Widget NewsWebView() {
+    return WebView(
+      initialUrl: contohURL,
+    );
+  }
 
   Widget CustomAppBar() {
     return Card(
