e4461009c9214465728e23ae63a5dfff7ff5f925
Kai-69 <ichihatsu1@gmail.com>
2021-11-22 11:04:02 +0700

    add Covid Data Section in Home Page

--- lib/persentation/pages/home_page.dart
+++ lib/persentation/pages/home_page.dart
@@ -60,6 +60,76 @@ class HomePage extends StatelessWidget {
       );
     }
 
+    Widget CovidDataSection(BuildContext context) {
+      Widget DataCovidItem(icon, angka, keterangan, iconColor) {
+        return Container(
+          width: 110,
+          height: 140,
+          padding: EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: kDeepGreen,
+              width: 1,
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 7, bottom: 10),
+                child: CircleAvatar(
+                  backgroundColor: iconColor,
+                  child: Icon(icon),
+                ),
+              ),
+              Text(
+                angka,
+                style: kHeading6,
+              ),
+              Text(
+                keterangan,
+                style: kHeading6.copyWith(fontWeight: FontWeight.w600),
+              )
+            ],
+          ),
+        );
+      }
+
+      return Container(
+        margin: EdgeInsets.all(10),
+        height: 180,
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 10, left: 10, right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Update Terkini",
+                    style: kSubtitle.copyWith(fontWeight: FontWeight.w600),
+                  ),
+                  Text("12 25 2021")
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                DataCovidItem(Icons.add_circle_outline_outlined, "4.000.000", "Positif",
+                    Colors.lightBlue.withOpacity(0.5)),
+                DataCovidItem(
+                    Icons.favorite, "4.000.000", "Sembuh", kLightGreen.withOpacity(0.5)),
+                DataCovidItem(
+                    Icons.dangerous_outlined, "4.000", "Mati", Colors.red.withOpacity(0.8)),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,7 +137,7 @@ class HomePage extends StatelessWidget {
         child: ListView(
           children: [
             HomePageHeader(context),
-            // CovidDataSection(context),
+            CovidDataSection(context),
             // HomePageBanner(context),
             // InformationSection(context),
           ],
