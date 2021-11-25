79018af72623efe0338672ebd9e12faa0da595b5
Kai-69 <ichihatsu1@gmail.com>
2021-11-25 10:33:46 +0700

    add register form in register page

--- lib/persentation/pages/register_page.dart
+++ lib/persentation/pages/register_page.dart
@@ -26,4 +26,107 @@ class RegisterPage extends StatelessWidget {
       ),
     );
   }
+
+  Widget registerForm() => Container(
+        margin: EdgeInsets.only(right: 35,left: 35, bottom: 40),
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              spreadRadius: 7,
+              blurRadius: 15,
+              offset: Offset(0, 0),
+            )
+          ],
+          borderRadius: BorderRadius.circular(20),
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [kLightGreen.withOpacity(0.6), kDeepGreen.withOpacity(0.7)],
+          ),
+        ),
+        child: Column(
+          children: [
+            Container(
+                padding: EdgeInsets.symmetric(vertical: 15),
+                child: Text("Register", style: kHeading4.copyWith(color: kWhite))),
+            TextField(
+              onChanged: (value) => userName = value,
+              decoration: InputDecoration(
+                hintText: 'Enter Your Name',
+                filled: true,
+                fillColor: kWhite,
+                prefixIcon: Icon(Icons.person),
+                border: OutlineInputBorder(
+                    borderSide: BorderSide.none, borderRadius: BorderRadius.circular(40)),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              onChanged: (value) => userEmail = value,
+              decoration: InputDecoration(
+                hintText: 'Enter Your Email',
+                filled: true,
+                fillColor: kWhite,
+                prefixIcon: Icon(Icons.email_rounded),
+                border: OutlineInputBorder(
+                    borderSide: BorderSide.none, borderRadius: BorderRadius.circular(40)),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              onChanged: (value) => userAddress = value,
+              decoration: InputDecoration(
+                hintText: 'Enter Your Address',
+                filled: true,
+                fillColor: kWhite,
+                prefixIcon: Icon(Icons.pin_drop),
+                border: OutlineInputBorder(
+                    borderSide: BorderSide.none, borderRadius: BorderRadius.circular(40)),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              obscureText: true,
+              onChanged: (value) => userPassword = value,
+              decoration: InputDecoration(
+                hintText: 'Enter Your Password',
+                filled: true,
+                fillColor: kWhite,
+                prefixIcon: Icon(Icons.lock),
+                border: OutlineInputBorder(
+                    borderSide: BorderSide.none, borderRadius: BorderRadius.circular(40)),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 60),
+                  primary: kWhite,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                  side: BorderSide(color: kWhite, width: 2),
+                ),
+                child: Text("Register", style: kHeading6)),
+            SizedBox(
+              height: 5,
+            ),
+            TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(primary: kWhite),
+                child: Text("Have an Account?", style: kSubtitle)),SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      );
 }
